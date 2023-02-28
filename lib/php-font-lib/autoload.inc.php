4543382befb1025bfbe56eeb3779fb302143aa65
Gilles Coissac <gilles@ideographe.fr>
2023-02-28 10:56:58 +0100

    Inclusion de la librairie php-font-lib: https://github.com/PhenX/php-font-lib

---
+++ lib/php-font-lib/autoload.inc.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package dompdf
+ * @link    https://github.com/PhenX/php-font-lib
+ * @author  Fabien Ménager <fabien.menager@gmail.com>
+ * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
+ */
+
+require_once __DIR__ . '/src/FontLib/Autoloader.php';
+
