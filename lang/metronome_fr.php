b40831a7cf88948d740022b3570fa467ece6740e
Gilles Coissac <gilles@ideographe.fr>
2023-02-28 10:50:32 +0100

    Creation du fichier lang FR

---
+++ lang/metronome_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	// C
+	'configurer' => 'Configuration du metronome',
+	'configurer_grid_columns' => 'colonnes',
+	'configurer_grid_gutter_width' => 'goutière',
+	'configurer_grille' => 'Grille',
+);
